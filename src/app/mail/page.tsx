27732bc272a394deed507c23f98e1c277914f662
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-03-23 16:53:03 +0530

    Adds base layout (#1)

---
+++ src/app/mail/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'Mail',
+}
+
+export default function Mail() {
+    return <h1>Mail Service</h1>
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'ghostwhite', padding: '1rem' }}>
+            Footer
+        </footer>
+    )
+}
