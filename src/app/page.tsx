48e0520e71a96ca60333511f6d0d34e1adae6c81
Mayank Pathak <onlinejudge95@gmail.com>
2024-03-23 20:34:38 +0530

    Adds responsive navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-    return <h1>Hello, World!!</h1>
+    return (
+        <>
+            <div className="container mx-auto px-4">
+                <h1>Hello World!!</h1>
+                <p>
+                    Dummy content for making the page longer for scroll effects
+                </p>
+                <div className="w-full h-screen bg-blue-300"></div>
+                <p>
+                    Dummy content for making the page longer for scroll effects
+                </p>
+            </div>
+        </>
+    )
 }
