169ddf19f1b08f474b9ebd81e9ad5782eba742b4
Mayank Pathak <onlinejudge95@gmail.com>
2024-03-23 17:09:10 +0530

    Adds CI file with linting disabled

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,18 @@ export default function Header() {
         <header style={{ backgroundColor: 'lightblue', padding: '1rem' }}>
             <nav>
                 <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/mail">Mail</Link></li>
-                    <li><Link href="/blog">Blog</Link></li>
-                    <li><Link href="/status">Status</Link></li>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/mail">Mail</Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link href="/status">Status</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
