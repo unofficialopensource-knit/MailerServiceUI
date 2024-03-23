cfbd713e1fbed6d264e8f7fe76b837839242e020
Mayank Pathak <onlinejudge95@gmail.com>
2024-03-23 16:52:24 +0530

    Adds base layout

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'Blog',
+}
+
+export default function Blog() {
+    return <h1>Blog</h1>
+}
---
+++ src/app/status/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'Status',
+}
+
+export default function Status() {
+    return <h1>Service status</h1>
+}
